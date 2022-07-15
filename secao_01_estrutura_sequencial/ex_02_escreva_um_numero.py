907df22e68f78da503f6b260f019fd6b3258e68b
Josue23 <josuerodrigues@gmail.com>
2022-07-15 07:24:04 -0300

    correção do exercício 02 da seção 01

--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -4,13 +4,16 @@ https://wiki.python.org.br/EstruturaSequencial
 
 Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 
-    >>> from secao_01_estrutura_sequencial import ex_02_escreva_um_numero
+    # >>> from secao_01_estrutura_sequencial import ex_02_escreva_um_numero
+    >>> import ex_02_escreva_um_numero
     >>> ex_02_escreva_um_numero.input = lambda k: '42'
     >>> ex_02_escreva_um_numero.escreva_um_numero()
     O número informado foi 42
 
 """
 
+
 def escreva_um_numero():
     """Escreva aqui em baixo a sua solução"""
-
+    numero = int(input('Por favor informe um número: '))
+    print(f'O número informado foi {numero}')
