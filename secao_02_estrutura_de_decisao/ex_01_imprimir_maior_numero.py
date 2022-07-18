536901cffa8f284dfc553acc654dd57ff0484acf
Josue23 <josuerodrigues@gmail.com>
2022-07-18 06:21:48 -0300

    correção do exercício 01 da seção 02

--- secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
+++ secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
@@ -18,6 +18,6 @@ Faça um Programa que peça dois números e imprima o maior deles.
 def maior_de_dois_numeros(x, y):
     """Escreva aqui em baixo a sua solução"""
     if x > y:
-        print(f'O maior número é: {x}')
+        print(f'{x}')
     else:
-        print(f'O maior número é: {y}')
+        print(f'{y}')
