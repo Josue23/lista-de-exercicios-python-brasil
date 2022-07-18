0fceec78072406d08c32f688fbfb8a91c2276dba
Josue23 <josuerodrigues@gmail.com>
2022-07-18 06:35:27 -0300

    correção do exercício 03 da seção 02

--- secao_02_estrutura_de_decisao/ex_03_F_ou_M.py
+++ secao_02_estrutura_de_decisao/ex_03_F_ou_M.py
@@ -18,3 +18,9 @@ Retorne: F - Feminino ou M - Masculino. Para quaisquer outros valores, retorne S
 
 def f_ou_m(sexo):
     """Escreva aqui em baixo a sua solução"""
+    if 'F':
+        return 'F - Feminino'
+    elif 'M':
+        return 'M - Masculino'
+    else:
+        return 'Sexo inválido'
