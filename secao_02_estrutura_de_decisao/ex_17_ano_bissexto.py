ffcfffb0d02222f03f102f8e063b0f69e933ca51
Josue23 <josuerodrigues@gmail.com>
2022-07-23 12:40:10 -0300

    correção do exercício 17 da seção 02

--- secao_02_estrutura_de_decisao/ex_17_ano_bissexto.py
+++ secao_02_estrutura_de_decisao/ex_17_ano_bissexto.py
@@ -20,3 +20,15 @@ bissexto.
 
 def eh_ano_bissexto(ano: int):
     """Escreva aqui em baixo a sua solução"""
+    if (ano % 4) == 0:
+        if (ano % 100) == 0:
+            if (ano % 400) == 0:
+                bissexto = True
+            else:
+                bissexto = False
+        else:
+            bissexto = True
+    else:
+        bissexto = False
+
+    return bissexto
