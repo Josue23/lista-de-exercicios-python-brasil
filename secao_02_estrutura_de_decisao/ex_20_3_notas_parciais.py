d7f22676fbb276010106f395f45d3c3d3278a761
Josue23 <josuerodrigues@gmail.com>
2022-07-25 12:25:59 -0300

    correção do exercício 20 da seção 02

--- secao_02_estrutura_de_decisao/ex_20_3_notas_parciais.py
+++ secao_02_estrutura_de_decisao/ex_20_3_notas_parciais.py
@@ -21,3 +21,7 @@ A mensagem "Aprovado com Distinção", se a média for igual a 10.
 
 def calcular_status(nota_1, nota_2, nota_3):
     """Escreva aqui em baixo a sua solução"""
+    media = (nota_1 + nota_2 + nota_3) / 3
+    resultado = 'Aprovado com Distinção' if media == 10 else 'Reprovado' if media < 7 else 'Aprovado'
+
+    return resultado
