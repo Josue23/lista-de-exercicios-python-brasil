1971f21bbdda90acebbb351b970968d803b98ca1
Josue23 <josuerodrigues@gmail.com>
2022-07-25 14:18:39 -0300

    correção do exercício 21 da seção 02

--- secao_02_estrutura_de_decisao/ex_21_troco.py
+++ secao_02_estrutura_de_decisao/ex_21_troco.py
@@ -25,5 +25,31 @@ uma nota de 5 e quatro notas de 1.
 """
 
 
+def rreplace(s, old, new, occurrence):
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
+
+
 def calcular_troco(valor: int) -> str:
     """Escreva aqui em baixo a sua solução"""
+
+    opcoes_de_notas = [100, 50, 10, 5, 1]
+
+    notas_100, restante = divmod(valor, 100)
+    notas_50, restante = divmod(restante, 50)
+    notas_10, restante = divmod(restante, 10)
+    notas_5, restante = divmod(restante, 5)
+    notas_1 = restante
+
+    lista_notas_int = [notas_100, notas_50, notas_10, notas_5, notas_1]
+    lista_notas_str = ['notas' if nota !=
+                       1 else 'nota' for nota in lista_notas_int]
+
+    mensagem = ''
+    for count, value in enumerate(lista_notas_int):
+        if value > 0:
+            mensagem += f'{lista_notas_int[count]} {lista_notas_str[count]} de R$ {opcoes_de_notas[count]}, '
+
+    mensagem = rreplace(mensagem, ', ', '', 1)
+
+    return f"{rreplace(mensagem, ', ', ' e ', 1)}"
