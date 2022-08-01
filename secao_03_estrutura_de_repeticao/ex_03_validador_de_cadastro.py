b91a354be175f802651dbccaef2154d4e0daa138
Josue23 <josuerodrigues@gmail.com>
2022-08-01 17:38:12 -0300

    correção do exercício 03 da seção 03

--- secao_03_estrutura_de_repeticao/ex_03_validador_de_cadastro.py
+++ secao_03_estrutura_de_repeticao/ex_03_validador_de_cadastro.py
@@ -40,3 +40,36 @@ Faça um programa que leia e valide as seguintes informações:
 
 def cadastrar_usuario(nome: str, idade: int, salario: float, sexo: str, estado_civil: str):
     """Escreva aqui em baixo a sua solução"""
+    cadastro_dict_bool = {
+        nome: len(nome) > 2,
+        idade: idade in range(151),
+        salario: salario > 0,
+        sexo: sexo in ['f', 'm'],
+        estado_civil: estado_civil in ['s', 'c', 'v', 'd']
+    }
+    dados_lista = ['nome', 'idade', 'salário', 'sexo', 'estado civil']
+
+    zipped = zip(cadastro_dict_bool.items(), dados_lista)
+
+    for (valor_dado_usuario, booleano), dado_usuario in zipped:
+        # if booleano == False:
+        if not booleano:
+            if dado_usuario == dados_lista[0]:
+                print(
+                    f'Erro: o {dado_usuario} precisa ter 3 letras ou mais, não pode ser {valor_dado_usuario}')
+            elif dado_usuario == dados_lista[1]:
+                print(
+                    f'Erro: a {dado_usuario} precisa estar entre 0 e 150, não pode ser {valor_dado_usuario}')
+            elif dado_usuario == dados_lista[2]:
+                print(
+                    f'Erro: o {dado_usuario} precisa ser positivo, não pode ser {valor_dado_usuario}')
+            elif dado_usuario == dados_lista[3]:
+                print(
+                    f'Erro: o {dado_usuario} precisa ser "m" ou "f", não pode ser "{valor_dado_usuario}"')
+            elif dado_usuario == dados_lista[4]:
+                print(
+                    f'Erro: o {dado_usuario} precisa ser "s", "c", "v" ou "d", não pode ser "{valor_dado_usuario}"')
+
+    cadastro_valores_lista = list(cadastro_dict_bool.values())
+    if all(p is True for p in cadastro_valores_lista):
+        print('Cadastro realizado com sucesso')
