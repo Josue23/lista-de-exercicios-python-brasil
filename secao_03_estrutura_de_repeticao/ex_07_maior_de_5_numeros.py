5322b53447f043869c6003749f39fe4478210089
Josue23 <josuerodrigues@gmail.com>
2022-08-02 12:19:50 -0300

    correção do exercício 07 da seção 03

--- secao_03_estrutura_de_repeticao/ex_07_maior_de_5_numeros.py
+++ secao_03_estrutura_de_repeticao/ex_07_maior_de_5_numeros.py
@@ -13,3 +13,10 @@ Faça um programa que leia 5 números e informe o maior número.
 
 def calcular_maior_numero(n1: int, n2: int, n3: int, n4: int, n5: int, ) -> int:
     """Escreva aqui em baixo a sua solução"""
+
+    numeros = [n1, n2, n3, n4, n5]
+    maior = numeros[0]
+    for numero in numeros[1:]:
+        if numero > maior:
+            maior = numero
+    print(maior)
