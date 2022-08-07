8acecd239b0ba983884b89261675b29cc2e67010
Josue23 <josuerodrigues@gmail.com>
2022-08-07 19:37:24 -0300

    correção do exercício 24 da seção 03

--- secao_03_estrutura_de_repeticao/ex_24_media_artmetica.py
+++ secao_03_estrutura_de_repeticao/ex_24_media_artmetica.py
@@ -1,3 +1,4 @@
+from _pydecimal import Decimal
 """
 Exercício 24 da seção de estrutura sequencial da Python Brasil:
 https://wiki.python.org.br/EstruturaDeRepeticao
@@ -18,3 +19,14 @@ Faça um programa que calcule o mostre a média aritmética de N notas.
 
 def calcular_media(*notas) -> float:
     """Escreva aqui em baixo a sua solução"""
+    if len(notas) == 0:
+        return 'É necessária ao menos uma nota para calcular a média'
+    else:
+        for nota in notas:
+            (soma := nota)
+        result = str(soma / len(notas))
+
+    '''remove os zeros ao lado direito do ponto decimal, depois remove o ponto decimal'''
+    result = result.rstrip('0').rstrip('.') if '.' in result else result
+
+    return result
