6891971fa3342f626b9ca7998fa715ee60ad1765
Josue23 <josuerodrigues@gmail.com>
2022-08-29 20:08:55 -0300

    correção do exercício 11 da seção 04

--- secao_04_exercicios_lista/ex_11_intercalacao_de_3_vetores.py
+++ secao_04_exercicios_lista/ex_11_intercalacao_de_3_vetores.py
@@ -19,3 +19,9 @@ Altere o programa anterior, intercalando 3 vetores.
 def intercalar(lista_1: list, lista_2: list, lista_3: list) -> list:
     """Escreva aqui em baixo a sua solução"""
 
+    if not lista_1:
+        print(lista_1)
+    else:
+        lista_4 = [item for lista in zip(lista_1, lista_2, lista_3) for item in lista]
+
+        print(lista_4)
