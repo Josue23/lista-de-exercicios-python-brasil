a6d9977b8a419b4e7dbee953e63b94cf32b068d4
Josue23 <josuerodrigues@gmail.com>
2022-08-31 11:20:28 -0300

    correção do exercício 09 da seção 06

--- secao_06_exercicios_strings/ex_09_validador_de_cpf_strings.py
+++ secao_06_exercicios_strings/ex_09_validador_de_cpf_strings.py
@@ -25,5 +25,34 @@ e dos caracteres de formatação.
 """
 
 
+def primeiro_digito_verificador(cpf):
+    # Valida o primeiro dígito verificador
+    sum_of_products = sum(int(a) * int(b)
+                          for a, b in zip(cpf[0:9], range(10, 1, -1)))
+    expected_digit = (sum_of_products * 10 % 11) % 10
+    if int(cpf[9]) != expected_digit:
+        return False
+    return True
+
+
+def segundo_digito_verificador(cpf):
+    # Valida o segundo dígito verificador
+    sum_of_products = sum(int(a) * int(b)
+                          for a, b in zip(cpf[0:10], range(11, 1, -1)))
+    expected_digit = (sum_of_products * 10 % 11) % 10
+    if int(cpf[10]) != expected_digit:
+        return False
+    return True
+
+
 def validar_cpf(cpf):
     """Escreva aqui em baixo a sua solução"""
+    # mantém apenas os números do CPF, ignorando pontuações
+    cpf = cpf.translate(str.maketrans('', '', '.-'))
+
+    if len(cpf) == 11:
+        if primeiro_digito_verificador(cpf) and segundo_digito_verificador(cpf):
+            return True
+        return False
+    else:
+        return False
